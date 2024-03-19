d7cef163462b6531ef8e23e0c180d4bfd74e872c
Theun van Vliet <theunis@gmail.com>
2024-03-19 11:33:29 +0100

    Formatting things

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -1,54 +1,65 @@
 return { -- Autoformat
-    'stevearc/conform.nvim',
-    enabled = true,
-    event = { "BufWritePre" },
-    cmd = { "ConformInfo" },
-    config = function()
-      require('conform').setup {
-        notify_on_error = false,
-        format_on_save = {
-          timeout_ms = 500,
-          lsp_fallback = true,
+  'stevearc/conform.nvim',
+  enabled = true,
+  event = { 'BufWritePre' },
+  cmd = { 'ConformInfo' },
+  config = function()
+    require('conform').setup {
+      notify_on_error = false,
+      format_on_save = {
+        timeout_ms = 500,
+        lsp_fallback = true,
+      },
+      formatters_by_ft = {
+        lua = { 'mystylua' },
+        python = { 'isort', 'black' },
+      },
+      formatters = {
+        mystylua = {
+          command = 'stylua',
+          args = { '--indent-type', 'Spaces', '--indent-width', '2', '-' },
         },
-        formatters_by_ft = {
-          lua = { 'mystylua' },
-          python = { 'isort', 'black' },
+      },
+    }
+    -- Customize the "injected" formatter
+    require('conform').formatters.injected = {
+      -- Set the options field
+      options = {
+        -- Set to true to ignore errors
+        ignore_errors = false,
+        -- Map of treesitter language to file extension
+        -- A temporary file name with this extension will be generated during formatting
+        -- because some formatters care about the filename.
+        lang_to_ext = {
+          bash = 'sh',
+          c_sharp = 'cs',
+          elixir = 'exs',
+          javascript = 'js',
+          julia = 'jl',
+          latex = 'tex',
+          markdown = 'md',
+          python = 'py',
+          ruby = 'rb',
+          rust = 'rs',
+          teal = 'tl',
+          r = 'r',
+          typescript = 'ts',
         },
-        formatters = {
-          mystylua = {
-            command = 'stylua',
-            args = { '--indent-type', 'Spaces', '--indent-width', '2', '-' },
-          },
-        },
-      }
-      -- Customize the "injected" formatter
-      require('conform').formatters.injected = {
-        -- Set the options field
-        options = {
-          -- Set to true to ignore errors
-          ignore_errors = false,
-          -- Map of treesitter language to file extension
-          -- A temporary file name with this extension will be generated during formatting
-          -- because some formatters care about the filename.
-          lang_to_ext = {
-            bash = 'sh',
-            c_sharp = 'cs',
-            elixir = 'exs',
-            javascript = 'js',
-            julia = 'jl',
-            latex = 'tex',
-            markdown = 'md',
-            python = 'py',
-            ruby = 'rb',
-            rust = 'rs',
-            teal = 'tl',
-            r = 'r',
-            typescript = 'ts',
-          },
-          -- Map of treesitter language to formatters to use
-          -- (defaults to the value from formatters_by_ft)
-          lang_to_formatters = {},
-        },
-      }
-    end,
-  }
+        -- Map of treesitter language to formatters to use
+        -- (defaults to the value from formatters_by_ft)
+        lang_to_formatters = {},
+      },
+      vim.api.nvim_create_user_command('Format', function(args)
+        local range = nil
+        if args.count ~= -1 then
+          local end_line = vim.api.nvim_buf_get_lines(0, args.line2 - 1, args.line2, true)[1]
+          range = {
+            start = { args.line1, 0 },
+            ['end'] = { args.line2, end_line:len() },
+          }
+        end
+        require('conform').format { async = true, lsp_fallback = true, range = range }
+      end, { range = true }),
+    }
+  end,
+}
--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -1,81 +1,121 @@
-local on_attach = require("util.lsp").on_attach
-local diagnostic_signs = require("util.lsp").diagnostic_signs
+local on_attach = require('util.lsp').on_attach
+local diagnostic_signs = require('util.lsp').diagnostic_signs
 
 local config = function()
-  require("neoconf").setup({})
-  local cmp_nvim_lsp = require("cmp_nvim_lsp")
-  local lspconfig = require("lspconfig")
+  require('neoconf').setup {}
+  local cmp_nvim_lsp = require 'cmp_nvim_lsp'
+  local lspconfig = require 'lspconfig'
 
   for type, icon in pairs(diagnostic_signs) do
-    local hl = "DiagnosticSign" .. type
-    vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
+    local hl = 'DiagnosticSign' .. type
+    vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = '' })
   end
 
   local capabilities = vim.lsp.protocol.make_client_capabilities()
-  capabilities = vim.tbl_deep_extend("force", capabilities, cmp_nvim_lsp.default_capabilities())
+  capabilities = vim.tbl_deep_extend('force', capabilities, cmp_nvim_lsp.default_capabilities())
   capabilities.textDocument.completion.completionItem.snippetSupport = true
   -- local capabilities = cmp_nvim_lsp.default_capabilities()
+  --
   capabilities.textDocument.foldingRange = {
     dynamicRegistration = false,
     lineFoldingOnly = true,
   }
 
   -- lua
-  lspconfig.lua_ls.setup({
+  lspconfig.lua_ls.setup {
     capabilities = capabilities,
     on_attach = on_attach,
     settings = { -- custom settings for lua
       Lua = {
         -- make the language server recognize "vim" global
         diagnostics = {
-          globals = { "vim" },
+          globals = { 'vim' },
         },
         workspace = {
           -- make language server aware of runtime files
           library = {
-            [vim.fn.expand("$VIMRUNTIME/lua")] = true,
-            [vim.fn.stdpath("config") .. "/lua"] = true,
+            [vim.fn.expand '$VIMRUNTIME/lua'] = true,
+            [vim.fn.stdpath 'config' .. '/lua'] = true,
           },
         },
       },
     },
-  })
+  }
+
+  -- also needs:
+  -- $home/.config/marksman/config.toml :
+  -- [core]
+  -- markdown.file_extensions = ["md", "markdown", "qmd"]
+  lspconfig.marksman.setup {
+    capabilities = capabilities,
+    filetypes = { 'markdown', 'quarto' },
+    root_dir = lspconfig.util.root_pattern('.git', '.marksman.toml', '_quarto.yml'),
+  }
+
+  local function get_quarto_resource_path()
+    local function strsplit(s, delimiter)
+      local result = {}
+      for match in (s .. delimiter):gmatch('(.-)' .. delimiter) do
+        table.insert(result, match)
+      end
+      return result
+    end
+
+    local f = assert(io.popen('quarto --paths', 'r'))
+    local s = assert(f:read '*a')
+    f:close()
+    return strsplit(s, '\n')[2]
+  end
+
+  local lua_library_files = vim.api.nvim_get_runtime_file('', true)
+  local lua_plugin_paths = {}
+  local resource_path = get_quarto_resource_path()
+  if resource_path == nil then
+    vim.notify_once 'quarto not found, lua library files not loaded'
+  else
+    table.insert(lua_library_files, resource_path .. '/lua-types')
+    table.insert(lua_plugin_paths, resource_path .. '/lua-plugin/plugin.lua')
+  end
 
   -- python
-  lspconfig.pyright.setup({
+  lspconfig.pyright.setup {
     capabilities = capabilities,
     on_attach = on_attach,
     settings = {
       pyright = {
         disableOrganizeImports = false,
         analysis = {
-          useLibraryCodeForTypes = true,
+          useLibraryCodeForTypes = false,
           autoSearchPaths = true,
-          diagnosticMode = "workspace",
-          autoImportCompletions = true,
+          diagnosticMode = 'workspace',
+          -- autoImportCompletions = true,
         },
       },
     },
-  })
+    root_dir = function(fname)
+      return lspconfig.util.root_pattern('.git', 'setup.py', 'setup.cfg', 'pyproject.toml', 'requirements.txt')(fname) or lspconfig.util.path.dirname(fname)
+    end,
+  }
 
   -- dart
-  lspconfig.dartls.setup({
+  lspconfig.dartls.setup {
     capabilities = capabilities,
     on_attach = on_attach,
-  })
+  }
 
-  local luacheck = require("efmls-configs.linters.luacheck")
-  local stylua = require("efmls-configs.formatters.stylua")
-  local flake8 = require("efmls-configs.linters.flake8")
-  local black = require("efmls-configs.formatters.black")
-  local dartanalyzer = require("linters.dart_analyze")
-  local dartfmt = require("efmls-configs.formatters.dartfmt")
+  local luacheck = require 'efmls-configs.linters.luacheck'
+  local stylua = require 'efmls-configs.formatters.stylua'
+  local flake8 = require 'efmls-configs.linters.flake8'
+  local black = require 'efmls-configs.formatters.black'
+  -- local darker = require("efmls-configs.formatters.darker")
+  local dartanalyzer = require 'linters.dart_analyze'
+  local dartfmt = require 'efmls-configs.formatters.dartfmt'
   -- configure efm server
-  lspconfig.efm.setup({
+  lspconfig.efm.setup {
     filetypes = {
-      "lua",
-      "python",
-      "dart",
+      'lua',
+      'python',
+      'dart',
     },
     init_options = {
       documentFormatting = true,
@@ -92,20 +132,20 @@ local config = function()
         dart = { dartanalyzer, dartfmt },
       },
     },
-  })
+  }
 end
 
 return {
-  "neovim/nvim-lspconfig",
+  'neovim/nvim-lspconfig',
   config = config,
   lazy = false,
   dependencies = {
-    "windwp/nvim-autopairs",
-    "williamboman/mason.nvim",
-    "creativenull/efmls-configs-nvim",
-    "hrsh7th/nvim-cmp",
-    "hrsh7th/cmp-buffer",
-    "hrsh7th/cmp-nvim-lsp",
-    { "j-hui/fidget.nvim", opts = {} },
+    'windwp/nvim-autopairs',
+    'williamboman/mason.nvim',
+    'creativenull/efmls-configs-nvim',
+    'hrsh7th/nvim-cmp',
+    'hrsh7th/cmp-buffer',
+    'hrsh7th/cmp-nvim-lsp',
+    { 'j-hui/fidget.nvim', opts = {} },
   },
 }
