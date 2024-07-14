f36f5289b6cd40f66f9f319676e4b14d1fe6a6a8
Theun van Vliet <theunis@gmail.com>
2024-07-14 16:56:07 +0200

    Fixes to obsidian and treesitter

--- lua/plugins/obsidian-nvim.lua
+++ lua/plugins/obsidian-nvim.lua
@@ -1,4 +1,4 @@
-function check_directory(dir_path, default_dir)
+local function check_directory(dir_path, default_dir)
   local exists = vim.fn.isdirectory(dir_path)
   if exists == 1 then
     return dir_path
@@ -8,10 +8,10 @@ function check_directory(dir_path, default_dir)
 end
 
 return {
-  'epwalsh/obsidian.nvim',
-  version = '*', -- recommended, use latest release instead of latest commit
+  "epwalsh/obsidian.nvim",
+  version = "*", -- recommended, use latest release instead of latest commit
   lazy = false,
-  ft = 'markdown',
+  ft = "markdown",
   -- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
   -- event = {
   --   -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
@@ -21,37 +21,37 @@ return {
   -- },
   dependencies = {
     -- Required.
-    'nvim-lua/plenary.nvim',
+    "nvim-lua/plenary.nvim",
 
     -- Optional
-    'hrsh7th/nvim-cmp',
-    'nvim-telescope/telescope.nvim',
+    "hrsh7th/nvim-cmp",
+    "nvim-telescope/telescope.nvim",
   },
   opts = {
     workspaces = {
       {
-        name = 'main',
+        name = "main",
         path = function()
           return check_directory(
-            '~/OneDrive-SchipholNederlandBV (Archive)/Documenten/Schiphol',
-            '/Users/theunvanvliet/Library/CloudStorage/GoogleDrive-theunis@gmail.com/My Drive/Notes'
+            "/Users/theun/OneDrive-SchipholNederlandBV (Archive)/Documenten/Schiphol",
+            "/Users/theunvanvliet/Library/CloudStorage/GoogleDrive-theunis@gmail.com/My Drive/Notes"
           )
         end,
       },
     },
     daily_notes = {
       -- Optional, if you keep daily notes in a separate directory.
-      folder = 'Daily',
+      folder = "Daily",
       -- Optional, if you want to change the date format for the ID of daily notes.
-      date_format = '%Y-%m-%d',
+      date_format = "%Y-%m-%d",
       -- Optional, if you want to change the date format of the default alias of daily notes.
-      alias_format = '%Y-%m-%d',
+      alias_format = "%Y-%m-%d",
       -- Optional, if you want to automatically insert a template from your template directory like 'daily.md'
-      template = 'Shutdown routine',
+      template = "Shutdown routine",
     },
     -- Optional, for templates (see below).
     templates = {
-      folder = 'Templates',
+      folder = "Templates",
       -- A map for custom variables, the key should be the variable and the value a function
       substitutions = {},
     },
@@ -63,10 +63,10 @@ return {
       -- Define how various check-boxes are displayed
       checkboxes = {
         -- NOTE: the 'char' value has to be a single character, and the highlight groups are defined below.
-        [' '] = { char = '󰄱', hl_group = 'ObsidianTodo' },
-        ['x'] = { char = '', hl_group = 'ObsidianDone' },
-        ['>'] = { char = '', hl_group = 'ObsidianRightArrow' },
-        ['~'] = { char = '󰰱', hl_group = 'ObsidianTilde' },
+        [" "] = { char = "󰄱", hl_group = "ObsidianTodo" },
+        ["x"] = { char = "", hl_group = "ObsidianDone" },
+        [">"] = { char = "", hl_group = "ObsidianRightArrow" },
+        ["~"] = { char = "󰰱", hl_group = "ObsidianTilde" },
         -- Replace the above with this if you don't have a patched font:
         -- [" "] = { char = "☐", hl_group = "ObsidianTodo" },
         -- ["x"] = { char = "✔", hl_group = "ObsidianDone" },
@@ -74,26 +74,26 @@ return {
         -- You can also add more custom ones...
       },
       -- Use bullet marks for non-checkbox lists.
-      bullets = { char = '•', hl_group = 'ObsidianBullet' },
-      external_link_icon = { char = '', hl_group = 'ObsidianExtLinkIcon' },
+      bullets = { char = "•", hl_group = "ObsidianBullet" },
+      external_link_icon = { char = "", hl_group = "ObsidianExtLinkIcon" },
       -- Replace the above with this if you don't have a patched font:
       -- external_link_icon = { char = "", hl_group = "ObsidianExtLinkIcon" },
-      reference_text = { hl_group = 'ObsidianRefText' },
-      highlight_text = { hl_group = 'ObsidianHighlightText' },
-      tags = { hl_group = 'ObsidianTag' },
-      block_ids = { hl_group = 'ObsidianBlockID' },
+      reference_text = { hl_group = "ObsidianRefText" },
+      highlight_text = { hl_group = "ObsidianHighlightText" },
+      tags = { hl_group = "ObsidianTag" },
+      block_ids = { hl_group = "ObsidianBlockID" },
       hl_groups = {
         -- The options are passed directly to `vim.api.nvim_set_hl()`. See `:help nvim_set_hl`.
-        ObsidianTodo = { bold = true, fg = '#f78c6c' },
-        ObsidianDone = { bold = true, fg = '#89ddff' },
-        ObsidianRightArrow = { bold = true, fg = '#f78c6c' },
-        ObsidianTilde = { bold = true, fg = '#ff5370' },
-        ObsidianBullet = { bold = true, fg = '#89ddff' },
-        ObsidianRefText = { underline = true, fg = '#c792ea' },
-        ObsidianExtLinkIcon = { fg = '#c792ea' },
-        ObsidianTag = { italic = true, fg = '#89ddff' },
-        ObsidianBlockID = { italic = true, fg = '#89ddff' },
-        ObsidianHighlightText = { bg = '#75662e' },
+        ObsidianTodo = { bold = true, fg = "#f78c6c" },
+        ObsidianDone = { bold = true, fg = "#89ddff" },
+        ObsidianRightArrow = { bold = true, fg = "#f78c6c" },
+        ObsidianTilde = { bold = true, fg = "#ff5370" },
+        ObsidianBullet = { bold = true, fg = "#89ddff" },
+        ObsidianRefText = { underline = true, fg = "#c792ea" },
+        ObsidianExtLinkIcon = { fg = "#c792ea" },
+        ObsidianTag = { italic = true, fg = "#89ddff" },
+        ObsidianBlockID = { italic = true, fg = "#89ddff" },
+        ObsidianHighlightText = { bg = "#75662e" },
       },
     },
     -- Optional, completion of wiki links, local markdown links, and tags using nvim-cmp.
