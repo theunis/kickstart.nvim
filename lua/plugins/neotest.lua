483437376e3e6cf5fe5ddf2e5a9e71691351bb5c
Theun van Vliet <theunis@gmail.com>
2024-08-22 10:54:46 +0200

    tests and debug in python

--- lua/plugins/neotest.lua
+++ lua/plugins/neotest.lua
@@ -1,23 +1,25 @@
 return {
-  'nvim-neotest/neotest',
+  "nvim-neotest/neotest",
   dependencies = {
-    'nvim-lua/plenary.nvim',
-    'antoinemadec/FixCursorHold.nvim',
-    'nvim-treesitter/nvim-treesitter',
-    'sidlatau/neotest-dart',
+    "nvim-lua/plenary.nvim",
+    "antoinemadec/FixCursorHold.nvim",
+    "nvim-treesitter/nvim-treesitter",
+    "sidlatau/neotest-dart",
+    "nvim-neotest/neotest-python",
   },
   config = function()
-    require('neotest').setup {
+    require("neotest").setup({
       adapters = {
-        require 'neotest-dart' {
-          command = 'flutter', -- Command being used to run tests. Defaults to `flutter`
+        require("neotest-python"),
+        require("neotest-dart")({
+          command = "flutter", -- Command being used to run tests. Defaults to `flutter`
           -- Change it to `fvm flutter` if using FVM
           -- change it to `dart` for Dart only tests
           use_lsp = false, -- When set Flutter outline information is used when constructing test name.
           -- Useful when using custom test names with @isTest annotation
           custom_test_method_names = {},
-        },
+        }),
       },
-    }
+    })
   end,
 }
