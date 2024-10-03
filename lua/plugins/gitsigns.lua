0e036c2bc2e671b9a834dd1addffb4d5a118610b
Theun van Vliet <theunis@gmail.com>
2024-10-03 08:34:02 +0200

    clean up code

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -1,50 +1,50 @@
 return {
   -- Adds git related signs to the gutter, as well as utilities for managing changes
-  "lewis6991/gitsigns.nvim",
-  event = { "BufReadPre", "BufNewFile" },
+  'lewis6991/gitsigns.nvim',
+  event = { 'BufReadPre', 'BufNewFile' },
   opts = {
     -- See `:help gitsigns.txt`
     signs = {
-      add = { text = "+" },
-      change = { text = "~" },
-      delete = { text = "_" },
-      topdelete = { text = "‾" },
-      changedelete = { text = "~" },
+      add = { text = '+' },
+      change = { text = '~' },
+      delete = { text = '_' },
+      topdelete = { text = '‾' },
+      changedelete = { text = '~' },
     },
     on_attach = function(bufnr)
-      local wk = require("which-key")
+      local wk = require 'which-key'
       local gs = package.loaded.gitsigns
       local mappings = {
         {
-          "<leader>tg",
-          name = "Git",
-          p = { gs.preview_hunk, "Preview Hunk", buffer = bufnr },
+          '<leader>tg',
+          group = 'Git',
         },
+        { '<leader>tgp', gs.preview_hunk, 'Preview Hunk', buffer = bufnr },
         {
-          "<leader>[c",
-          name = "Previous",
+          '<leader>[c',
+          name = 'Previous',
           function()
             if vim.wo.diff then
-              return "[c"
+              return '[c'
             end
             vim.schedule(gs.prev_hunk)
-            return "<Ignore>"
+            return '<Ignore>'
           end,
-          "Git Hunk",
+          'Git Hunk',
           expr = true,
           buffer = bufnr,
         },
         {
-          "<leader>]c",
-          name = "Next",
+          '<leader>]c',
+          name = 'Next',
           function()
             if vim.wo.diff then
-              return "c"
+              return 'c'
             end
             vim.schedule(gs.next_hunk)
-            return "<Ignore>"
+            return '<Ignore>'
           end,
-          "Git Hunk",
+          'Git Hunk',
           expr = true,
           buffer = bufnr,
         },
