f1a2ed7effe0c1d5a903d3cecae5df68da0781e9
Theun van Vliet <theunis@gmail.com>
2024-10-09 18:55:10 +0200

    remove harpoon C-a keybinding

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -17,17 +17,14 @@ return {
     --   harpoon.ui:toggle_quick_menu(harpoon:list())
     -- end)
 
-    vim.keymap.set('n', '<C-a>', function()
+    vim.keymap.set('n', '<C-s>', function()
       harpoon:list():select(1)
     end)
-    vim.keymap.set('n', '<C-s>', function()
+    vim.keymap.set('n', '<C-f>', function()
       harpoon:list():select(2)
     end)
-    vim.keymap.set('n', '<C-f>', function()
-      harpoon:list():select(3)
-    end)
     vim.keymap.set('n', '<C-g>', function()
-      harpoon:list():select(4)
+      harpoon:list():select(3)
     end)
 
     -- Toggle previous & next buffers stored within Harpoon list
